cd1f65df5225a1096c2ddb3e2580a73167076b8e
Matheus Gois <matheusdc77@gmail.com>
2018-01-19 14:58:33 -0200

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  dados1: Array<any> = ['um','dois','tres','quatro','cinco'];
+  dados2: Array<any> = ['ator','novela','navalha','rei','mafioso'];
+  tenta: any;
+  inserido: Array<any> = [];
+  removido: Array<any> = [];
+
+  constructor(){
+  }
+
+  onetotwo(i) {
+    const tenta = this.dados1.splice(i, 1);
+    this.dados2.push(tenta);
+    this.inserido.push(tenta);
+  }
+
+  twotoone(i) {
+    const tenta = this.dados2.splice(i, 1);
+    this.dados1.push(tenta);
+    this.removido.push(tenta);
+  }
+
+  verification(){
+    console.log("valores inseridos de A -> B: ", this.inserido);
+    console.log("valores remodivod de B <- A: ", this.removido);
+  }
 }
